832a47a762cd2036a9c7e82457b4597874fe33a3
Neytred <ls.ivanova91@gmail.com>
2024-09-08 16:04:22 +0900

    ДЗ "Введение в Maven и Spring Boot"

---
+++ src/main/java/Homeworks/Calculator/exception/DivideByZeroException.java
@@ -0,0 +1,11 @@
+package Homeworks.Calculator.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class DivideByZeroException extends RuntimeException{
+    public DivideByZeroException(String message) {
+        super(message);
+    }
+}
